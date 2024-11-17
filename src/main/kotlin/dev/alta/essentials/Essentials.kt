90e827d18e8afe61b35554e5936ffa1cea50d8fc
cupofteame <malwarexxx@protonmail.com>
2024-11-17 11:22:26 -0600

    command completions

--- src/main/kotlin/dev/alta/essentials/Essentials.kt
+++ src/main/kotlin/dev/alta/essentials/Essentials.kt
@@ -1,12 +1,18 @@
 package dev.alta.essentials
 
 import co.aikar.commands.PaperCommandManager
+import co.aikar.commands.CommandCompletions
+import co.aikar.commands.BukkitCommandCompletionContext
 import dev.alta.essentials.utils.annotations.AnnotationUtils
 import dev.alta.essentials.utils.config.ConfigManager
 import me.lucko.helper.plugin.ExtendedJavaPlugin
 import net.luckperms.api.LuckPerms
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import net.luckperms.api.node.NodeType
+import net.luckperms.api.node.types.PermissionNode
+import org.bukkit.permissions.Permission
+import org.bukkit.Bukkit
 
 class Essentials : ExtendedJavaPlugin() {
     
@@ -35,6 +41,25 @@ class Essentials : ExtendedJavaPlugin() {
         // Rank completions
         commandManager.commandCompletions.registerAsyncCompletion("ranks") { _ ->
             luckPerms.groupManager.loadedGroups.map { it.name }
+        }
+        
+        // Permission completions
+        commandManager.commandCompletions.registerAsyncCompletion("permissions") { _ ->
+            Bukkit.getPluginManager().permissions
+                .map { it.name }
+                .sorted()
+        }
+        
+        // Rank-specific permission completions
+        commandManager.commandCompletions.registerAsyncCompletion("rankperms") { context: BukkitCommandCompletionContext ->
+            val rankName = context.input.split(" ").firstOrNull()
+            rankName?.let { name ->
+                luckPerms.groupManager.getGroup(name)
+                    ?.nodes
+                    ?.filterIsInstance<PermissionNode>()
+                    ?.map { it.permission }
+                    ?.sorted()
+            } ?: emptyList()
         }
         
         AnnotationUtils.registerAll(this)
